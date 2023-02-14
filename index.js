9b1131724a1a88c433be398266e02779467f43a3
jayasuryablue <121051170+jayasuryablue@users.noreply.github.com>
2023-02-14 20:36:07 +0530

    Navigation in mobile

---
+++ index.js
@@ -0,0 +1,21 @@
+// javascript code goes here// javascript code goes hereconst contents = document.querySelectorAll('.content')
+const listItems = document.querySelectorAll("nav ul li");
+const contents = document.querySelectorAll(".content");
+
+listItems.forEach((item, idx) => {
+  item.addEventListener("click", () => {
+    hideAllContents();
+    hideAllItems();
+
+    item.classList.add("active");
+    contents[idx].classList.add("show");
+  });
+});
+
+function hideAllContents() {
+  contents.forEach((content) => content.classList.remove("show"));
+}
+
+function hideAllItems() {
+  listItems.forEach((item) => item.classList.remove("active"));
+}
